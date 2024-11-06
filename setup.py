c1a6e605ccb53de5fc1bd477863a801687c17493
JuanLoaiza007 <juan.loaiza.santiago@correounivalle.edu.co>
2024-11-06 15:56:20 -0500

    feat: Implement setup verification process to stop if error

--- setup.py
+++ setup.py
@@ -8,11 +8,16 @@ requirements_file = "requirements.txt"
 def main():
     print("\nIniciando instalación y configuración del proyecto.\nEspere por favor...")
 
-    check_requirements_file()
-    create_virtual_env()
-    update_pip_and_setuptools()
-    install_dependencies()
-    init_postgres_config()
+    if not check_requirements_file():
+        return
+    if not create_virtual_env():
+        return
+    if not update_pip_and_setuptools():
+        return
+    if not install_dependencies():
+        return
+    if not init_postgres_config():
+        return
 
     print(
         "\n\n[FIN] Proceso completado."
@@ -25,13 +30,17 @@ def main():
 
 def check_requirements_file():
     if not os.path.exists(requirements_file):
-        print(f"\nEl archivo {requirements_file} no existe.")
-        sys.exit(1)
+        print(f"\n[ERROR] El archivo {requirements_file} no existe.")
+        return False
+    return True
 
 
 def create_virtual_env():
     print("\nCreando entorno virtual...")
-    os.system(f"{sys.executable} -m venv {env_name}")
+    if os.system(f"{sys.executable} -m venv {env_name}") != 0:
+        print("\n[ERROR] Falló la creación del entorno virtual.")
+        return False
+    return True
 
 
 def generate_python_executable():
@@ -45,12 +54,16 @@ def generate_python_executable():
 def update_pip_and_setuptools():
     python_executable = generate_python_executable()
     print(f"\nActualizando pip y setuptools en el entorno virtual...")
-    os.system(f"{python_executable} -m ensurepip --upgrade")
-    os.system(f"{python_executable} -m pip install --upgrade setuptools")
+    if (
+        os.system(f"{python_executable} -m ensurepip --upgrade") != 0
+        or os.system(f"{python_executable} -m pip install --upgrade setuptools") != 0
+    ):
+        print("\n[ERROR] Falló la actualización de pip y setuptools.")
+        return False
+    return True
 
 
 def install_dependencies():
-
     # Flags
     use_cache = True
     use_binary = True
@@ -64,9 +77,15 @@ def install_dependencies():
         + f"\nInstalando dependencias de Python desde {requirements_file}...\n"
     )
 
-    os.system(
-        f"{python_executable} -m pip install {cache} {binary} -r {requirements_file}"
-    )
+    if (
+        os.system(
+            f"{python_executable} -m pip install {cache} {binary} -r {requirements_file}"
+        )
+        != 0
+    ):
+        print("\n[ERROR] Falló la instalación de dependencias.")
+        return False
+    return True
 
 
 def init_postgres_config():
@@ -76,20 +95,22 @@ def init_postgres_config():
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    if os.system(f"git clone {repo_url} {target_dir}") == 0:
-        git_dir = os.path.join(target_dir, ".git")
-        if os.path.exists(git_dir):
-            if os.name == "nt":
-                os.system(f"rmdir /S /Q {git_dir}")
-            else:
-                os.system(f"rm -rf {git_dir}")
+    if os.system(f"git clone {repo_url} {target_dir}") != 0:
         print(
-            f"\nEl archivo de configuración se ha creado en: {os.path.abspath(target_dir)}"
+            "\n[ERROR] Falló la clonación del archivo de configuración. Asegúrate de tener 'git' instalado."
         )
-    else:
-        print(
-            f"\nError al clonar el archivo de configuración. Asegúrate de tener 'git' instalado."
-        )
+        return False
+
+    git_dir = os.path.join(target_dir, ".git")
+    if os.path.exists(git_dir):
+        if os.name == "nt":
+            os.system(f"rmdir /S /Q {git_dir}")
+        else:
+            os.system(f"rm -rf {git_dir}")
+    print(
+        f"\nEl archivo de configuración se ha creado en: {os.path.abspath(target_dir)}"
+    )
+    return True
 
 
 if __name__ == "__main__":
